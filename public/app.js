fad73078a48f313b203396275ed43a6bbfc31032
MrBowis <02alejo20@gmail.com>
2025-08-12 12:04:06 -0500

    deploy firebase with changes

--- public/app.js
+++ public/app.js
@@ -2,4 +2,4 @@ function sum(a, b){
     return a + b;
 }
 
-document.getElementById("out").textContent = `1 + 2 = ${sum(1, 2)}`;
+document.getElementById('out').textContent = `1 + 2 = ${sum(1, 2)}`;
